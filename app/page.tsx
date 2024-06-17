2254479c6eaf07c9a0e747f6cfb14cc4f26d8a7c
novaepitech <mateo.cabrera@epitech.eu>
2024-06-18 01:32:55 +0200

    feat: magic ui text flip between Nova and Mateo

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
 import Image from "next/image";
-import photoPro from "./assets/photo-pro_mirrored.png";
+import photoPro from "./assets/photo-pro-mateo.png";
 import { Button } from "@/components/ui/button";
+import WordRotate from "@/components/magicui/word-rotate";
 
 export default function Home() {
   return (
     <>
       <section className="mt-24">
         <div className="flex flex-between items-center">
-          <div>
+          <div className="w-3/5">
             <p>Etudiant développeur</p>
             <h1 className="mt-2 text-7xl">Salut, c'est</h1>
-            <h1 className="mb-8 text-7xl text-primary">Mateo Cabrera</h1>
+            <WordRotate
+              className="mb-8 text-7xl text-primary"
+              words={["Mateo Cabrera", "Nova"]}
+              duration={3500}
+            />
             <p className="font-thin">Je construis des choses avec ma souris et mon clavier.</p>
           </div>
           <div>
